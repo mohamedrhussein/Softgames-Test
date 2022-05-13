93920161bb5faa9efdaeb6c7c490b5268aa5646c
Mohamed Rabie <mohamed.rabie.hussein@gmail.com>
2022-05-13 15:26:30 +0200

    Load Html File in UIWebView View and center it

--- Softgames@Test/ViewController.swift
+++ Softgames@Test/ViewController.swift
@@ -6,14 +6,46 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    
+    // Outlet
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.loadHtmlFile()
+        
         // Do any additional setup after loading the view.
     }
-
-
+    
+    // Load Html File
+    private func loadHtmlFile(){
+        // Adding html content
+        do {
+            guard let filePath = Bundle.main.path(forResource: "softgames", ofType: "html")
+            else {
+                // File Error
+                print ("File reading error")
+                return
+            }
+            
+            let contents =  try String(contentsOfFile: filePath, encoding: .utf8)
+            let baseUrl = URL(fileURLWithPath: filePath)
+            webView.loadHTMLString(contents as String, baseURL: baseUrl)
+        }
+        catch {
+            print ("File HTML error")
+        }
+    }
+    
+    // Create Sync Button Action
+    // Recive Name Concate First and Last and Pass it to Html File
+    
+    
+    // Create Async Button Action with delay 5 seconds
+    // Recive DateOfBirth and Pass it to Html File after delay
 }
 
